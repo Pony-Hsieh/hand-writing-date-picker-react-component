e07b4d64bcaf67b612d69e660d48521d74a63b10
Pony Hsieh <lbjwin@gmail.com>
2024-03-21 17:18:25 +0800

    feat: 更新 App
    1. 引入 Calender 元件
    2. 新增標題
    3. 新增樣式

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useState } from 'react';
+import Calendar from './components/Calender';
+import dayjs from 'dayjs';
+import './styles/App.scss';
+
 function App() {
+  const [selectedDates, setSelectedDates] = useState([]);
+  const [selectedDates2, setSelectedDates2] = useState([]);
+
   return (
-    <>
-      <div></div>
-    </>
+    <div className="wrapper">
+      <h1>This is a page that includes Calender Component</h1>
+
+      <div className="calendarWrapper">
+        <h2>init date: today</h2>
+        <Calendar
+          initDate={dayjs()}
+          selectedDates={selectedDates}
+          setSelectedDates={setSelectedDates}
+        />
+      </div>
+
+      <div className="calendarWrapper">
+        <h2>init date: 2022-07-22</h2>
+        <Calendar
+          initDate={dayjs('2022-07-22')}
+          selectedDates={selectedDates2}
+          setSelectedDates={setSelectedDates2}
+        />
+      </div>
+    </div>
   );
 }
 
