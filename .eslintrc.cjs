7186b53285470f08b7791bfa1e5196096dac6c5d
Pony Hsieh <lbjwin@gmail.com>
2024-03-21 16:33:02 +0800

    fix: 修正 prettier 與 eslint 整合問題

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -19,4 +20,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
