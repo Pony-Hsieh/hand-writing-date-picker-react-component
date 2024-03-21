481e702d1b220fbbc4135e8a3243939911e7b4a7
Pony Hsieh <lbjwin@gmail.com>
2024-03-21 19:08:20 +0800

    feat: 在 App 引入元件並新增頁面文案

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
-import Calendar from './components/Calender';
+import Calendar1 from './components/Calendar1';
+import Calendar2 from './components/Calendar2';
 import dayjs from 'dayjs';
 import './styles/App.scss';
 
 function App() {
-  const [selectedDates, setSelectedDates] = useState([]);
+  const [selectedDates1, setSelectedDates1] = useState([]);
   const [selectedDates2, setSelectedDates2] = useState([]);
 
   return (
@@ -12,21 +13,41 @@ function App() {
       <h1>This is a page that includes Calendar Component</h1>
 
       <div className="calendarWrapper">
-        <h2>init date: today</h2>
-        <Calendar
+        <h2>Task - 1</h2>
+        <Calendar1
           initDate={dayjs()}
-          selectedDates={selectedDates}
-          setSelectedDates={setSelectedDates}
+          selectedDates={selectedDates1}
+          setSelectedDates={setSelectedDates1}
         />
+        <p>
+          selected date range:
+          <br />
+          {selectedDates1.length > 0 ? (
+            <>
+              {selectedDates1?.[0].format('YYYY/MM/DD')} -{' '}
+              {selectedDates1?.[1].format('YYYY/MM/DD')}
+            </>
+          ) : null}
+        </p>
       </div>
 
       <div className="calendarWrapper">
-        <h2>init date: 2022-07-22</h2>
-        <Calendar
-          initDate={dayjs('2022-07-22')}
+        <h2>Task - 2</h2>
+        <Calendar2
+          initDate={dayjs()}
           selectedDates={selectedDates2}
           setSelectedDates={setSelectedDates2}
         />
+        <p>
+          selected date range:
+          <br />
+          {selectedDates2.length > 0 ? (
+            <>
+              {selectedDates2?.[0].format('YYYY/MM/DD')} -{' '}
+              {selectedDates2?.[1].format('YYYY/MM/DD')}
+            </>
+          ) : null}
+        </p>
       </div>
     </div>
   );
